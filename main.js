151cd1fc746472cfb02351e5324c56e51c6b7d26
Aryan-Jade <76610755+Aryan-Jade@users.noreply.github.com>
2021-07-12 14:58:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+window.location = "index.html";
+
+function tv(){
+    window.location = "index.html";
+}
+
+function laptop(){
+    window.location = "index.html";
+}
+
+function bottle(){
+    window.location = "index.html";
+}
+
+function bed(){
+    window.location = "index.html";
+}
+
+function basket(){
+    window.location = "index.html";
+}
